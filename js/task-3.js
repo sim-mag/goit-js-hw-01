fc2600c6c99a3db728c4ada1593f1478bd93faa8
sim-mag <79795891+sim-mag@users.noreply.github.com>
2024-09-07 13:47:23 +0200

    updated tasks

--- js/task-3.js
+++ js/task-3.js
@@ -11,6 +11,8 @@ function getElementWidth(content, padding, border){
     return totalWidth;
 }
 
+console.log("These are the results of Task 3:");
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
